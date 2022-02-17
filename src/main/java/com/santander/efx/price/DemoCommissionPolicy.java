908e9a6dcf6d4f06528a0ba16ade52d94a4707f1
Konrad K <konrad.kaczkowski@o2.pl>
2022-02-17 11:05:35 +0100

    Small improvement in DemoCommisionPolicy + fixed a typo

--- src/main/java/com/santander/efx/price/DemoCommissionPolicy.java
+++ src/main/java/com/santander/efx/price/DemoCommissionPolicy.java
@@ -9,7 +9,10 @@ import java.math.RoundingMode;
  * Adds 0.1% to Ask and subtracts 0.1% from Bid
  */
 @Component
-public class DemoCommisionPolicy implements CommisionPolicy {
+public class DemoCPolicy implements CommisionPolicy {
+
+    private static final BigDecimal ASK_FACTOR = new BigDecimal("1.001");
+    private static final BigDecimal BID_FACTOR = new BigDecimal("0.999");
 
     @Override
     public Price apply(Price price) {
@@ -17,13 +20,13 @@ public class DemoCommisionPolicy implements CommisionPolicy {
                 .id(price.getId())
                 .instrumentName(price.getInstrumentName())
                 .timestamp(price.getTimestamp())
-                .ask(addPercentage(0.001, price.getAsk()))
-                .bid(addPercentage(-0.001, price.getBid()))
+                .ask(multiplyWithScale(price.getAsk(), ASK_FACTOR))
+                .bid(multiplyWithScale(price.getBid(), BID_FACTOR))
                 .build();
     }
 
-    private BigDecimal addPercentage(double percentage, BigDecimal value) {
-        return value.multiply(new BigDecimal(1.0d + percentage))
+    private BigDecimal multiplyWithScale(BigDecimal value1, BigDecimal value2) {
+        return value1.multiply(value2)
                 .setScale(Price.SCALE, RoundingMode.HALF_EVEN);
     }
 }
