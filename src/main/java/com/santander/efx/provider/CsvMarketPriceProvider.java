a5fa568c742f6f70f575066abf0ce9b47563123e
Konrad K <konrad.kaczkowski@o2.pl>
2022-02-17 11:17:11 +0100

    Added exception logging in PriceProvider

--- src/main/java/com/santander/efx/provider/CsvMarketPriceProvider.java
+++ src/main/java/com/santander/efx/provider/CsvMarketPriceProvider.java
@@ -1,5 +1,6 @@
 package com.santander.efx.provider;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
@@ -7,14 +8,21 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.stream.BaseStream;
 
+@Slf4j
 @Service
 public class CsvMarketPriceProvider implements MarketPriceProvider {
 
     @Override
     public Flux<String> getPrices() {
-        return Flux.using(() -> Files.lines(Paths.get("src", "main","resources","messages.csv")),
-                Flux::fromStream,
-                BaseStream::close
-        );
+        try {
+            return Flux.using(() -> Files.lines(Paths.get("src", "main", "resources", "messages.csv")),
+                    Flux::fromStream,
+                    BaseStream::close
+            );
+
+        } catch (Exception ex) {
+            log.error("Error while parsing messages", ex);
+        }
+        return Flux.empty();
     }
 }
